63ec864be3c7c8af8a7b863cae5b3d6d58835433
ivargas1030 <56612683+ivargas1030@users.noreply.github.com>
2020-01-03 19:59:14 -0600

    given code

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+init()};
